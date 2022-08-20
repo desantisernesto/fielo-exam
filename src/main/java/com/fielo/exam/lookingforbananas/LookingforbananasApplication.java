7b6a7384a5a61fbbc62eca774c386652f2e65126
Ernesto De Santis <ernesto.desanti@venhapranuvem.com.br>
2022-08-20 12:19:57 -0300

    Initial Project Commit

---
+++ src/main/java/com/fielo/exam/lookingforbananas/LookingforbananasApplication.java
@@ -0,0 +1,13 @@
+package com.fielo.exam.lookingforbananas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookingforbananasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LookingforbananasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fielo/exam/lookingforbananas/LookingforbananasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fielo.exam.lookingforbananas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookingforbananasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
